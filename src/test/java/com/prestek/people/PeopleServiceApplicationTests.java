46e001930d54d22135ceed432e913bc9d4b6d299
JAGBytes <jorge.gamboa-s@mail.escuelaing.edu.co>
2025-12-10 11:21:58 -0500

    feat test userEntity

--- src/test/java/com/prestek/people/PeopleServiceApplicationTests.java
+++ src/test/java/com/prestek/people/PeopleServiceApplicationTests.java
@@ -2,8 +2,19 @@ package com.prestek.people;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"CLERK_ISSUER=https://test-issuer.clerk.accounts.dev",
+		"CLERK_JWKS_URL=https://test-issuer.clerk.accounts.dev/.well-known/jwks.json",
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"ALLOWED_ORIGINS_HTTP=http://localhost:3000",
+		"ALLOWED_ORIGINS_HTTPS=https://localhost:3000"
+})
 class PeopleServiceApplicationTests {
 
 	@Test
